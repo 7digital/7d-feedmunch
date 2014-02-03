96c12a0fbe4a8648fc6d62841e5f5d7231b6f7ee
greg Sochanik <greg@sochanik.com>
2014-02-03 21:23:06 +0000

    First steps towards a generic field filtering class

--- src/SevenDigital.FeedMunch.Unit.Tests/FilterParseTests.cs
+++ src/SevenDigital.FeedMunch.Unit.Tests/FilterParseTests.cs
@@ -1,9 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using SevenDigital.Api.FeedReader.Feeds.Schema;
 
 namespace SevenDigital.FeedMunch.Unit.Tests
 {
+	[TestFixture]
+	public class FilteringTest
+	{
+		[Test]
+		public void Tets()
+		{
+			var terence = new Artist
+			{
+				name = "terence"
+			};
+
+			var bill = new Artist
+			{
+				name = "bill"
+			};
+
+			var artists = new List<object>
+			{
+				terence,
+				bill
+			};
+
+			var filter = new Filter("name=bill");
+
+			var filteredArtists = artists.Where(x =>
+			{
+				var type = x.GetType();
+				var propertyInfo = type.GetProperty(filter.FieldName);
+				var methodInfo = propertyInfo.GetGetMethod();
+				var invoke = methodInfo.Invoke(x, null);
+				return (string)invoke == filter.Values.First();
+			}).Cast<Artist>().ToList();
+
+			Assert.That(filteredArtists.Count, Is.EqualTo(1));
+			Assert.That(filteredArtists[0].name, Is.EqualTo("bill"));
+		}
+	}
 
 	[TestFixture]
 	public class FilterParseTests
