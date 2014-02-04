77150e8c84f91a4bd0953416ef3a0c2239a5550b
greg Sochanik <greg@sochanik.com>
2014-02-04 10:54:11 +0000

    Added ApplyToRow method to filter and tests around Artist object

---
+++ src/SevenDigital.FeedMunch.Unit.Tests/ArtistFilteringTest.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SevenDigital.Api.FeedReader.Feeds.Schema;
+
+namespace SevenDigital.FeedMunch.Unit.Tests
+{
+	[TestFixture]
+	public class ArtistFilteringTest
+	{
+		private IEnumerable<Artist> _artists;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_artists = Artists();
+		}
+
+		[Test]
+		public void Can_find_row_with_single_value_if_exists()
+		{
+			var filter = new Filter("name=mandela");
+
+			var filteredArtists = _artists.Where(filter.ApplyFilterToRow).ToList();
+
+			Assert.That(filteredArtists.Count, Is.EqualTo(1));
+			Assert.That(filteredArtists[0].name, Is.EqualTo("mandela"));
+		}
+
+		[Test]
+		public void Can_find_row_with_range_of_values()
+		{
+			var filter = new Filter("tags=india,south-africa");
+
+			var filteredArtists = _artists.Where(filter.ApplyFilterToRow).ToList();
+
+			Assert.That(filteredArtists.Count, Is.EqualTo(2));
+			Assert.That(filteredArtists[0].name, Is.EqualTo("gandhi"));
+			Assert.That(filteredArtists[1].name, Is.EqualTo("mandela"));
+		}
+
+		[Test]
+		public void Can_find_row_with_negative_single_value()
+		{
+			var filter = new Filter("tags!=third-reich");
+
+			var filteredArtists = _artists.Where(filter.ApplyFilterToRow).ToList();
+
+			Assert.That(filteredArtists.Count, Is.EqualTo(2));
+			Assert.That(filteredArtists[0].name, Is.EqualTo("gandhi"));
+			Assert.That(filteredArtists[1].name, Is.EqualTo("mandela"));
+		}
+
+		[Test]
+		public void Can_find_row_with_negative_range_of_values()
+		{
+			var filter = new Filter("tags!=india,south-africa");
+
+			var filteredArtists = _artists.Where(filter.ApplyFilterToRow).ToList();
+
+			Assert.That(filteredArtists.Count, Is.EqualTo(1));
+			Assert.That(filteredArtists[0].name, Is.EqualTo("hitler"));
+		}
+
+		private static IEnumerable<Artist> Artists()
+		{
+			var gandhi = new Artist
+			{
+				name = "gandhi",
+				popularity = "1",
+				tags = "india"
+			};
+
+			var mandela = new Artist
+			{
+				name = "mandela",
+				popularity = "1",
+				tags = "south-africa"
+			};
+
+			var hitler = new Artist
+			{
+				name = "hitler",
+				popularity = "0.1",
+				tags = "third-reich"
+			};
+
+			var artists = new List<Artist>
+			{
+				gandhi,
+				mandela,
+				hitler
+			};
+			return artists;
+		}
+	}
+}
