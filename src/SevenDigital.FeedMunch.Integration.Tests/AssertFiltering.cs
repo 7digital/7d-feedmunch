3df4bd5a34bf73429a72dccccd0528a7b45fcd9d
greg Sochanik <greg@sochanik.com>
2014-02-05 17:04:16 +0000

    Added an int test for filtering a local track update feed

---
+++ src/SevenDigital.FeedMunch.Integration.Tests/AssertFiltering.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using DeCsv;
+using NUnit.Framework;
+
+namespace SevenDigital.FeedMunch.Integration.Tests
+{
+	public class AssertFiltering
+	{
+		public static void IsAsExpected<T>(string outputFile, Func<T, bool> filteringPredicate)
+		{
+			using (var fileStream = File.OpenRead(outputFile))
+			{
+				using (var gZipStream = new GZipStream(fileStream, CompressionMode.Decompress))
+				{
+					var deSerialize = CsvDeserialize.DeSerialize<T>(gZipStream).ToList();
+					Assert.That(deSerialize.Any(filteringPredicate), Is.True, "No items matching filtering predicate are found");
+					Assert.That(deSerialize.All(filteringPredicate), Is.True, "Not all items match filtering predicate");
+				}
+			}
+		}
+	}
+}
