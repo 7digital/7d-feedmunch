7068e767f70e4989d339bba0e38be1ec4903baaf
greg Sochanik <greg@sochanik.com>
2014-02-06 14:10:09 +0000

    Removed some dead code

---
+++ src/FeedMuncher/ConsoleFilePolling.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace FeedMuncher
+{
+	public static class ConsoleFilePolling
+	{
+		public static Timer GenerateFileSizePollingTimer(string pathOfFileToPoll, int pollInterval)
+		{
+			return new Timer(x =>
+			{
+				if (!File.Exists(pathOfFileToPoll))
+				{
+					return;
+				}
+
+				var fileInfo = new FileInfo(pathOfFileToPoll);
+				var length = fileInfo.Length;
+				Console.CursorLeft = 0;
+				Console.WriteLine(length);
+				Console.CursorTop --;
+				Console.CursorVisible = false;
+
+			}, null, 0, pollInterval);
+		}
+	}
+}
--- src/SevenDigital.FeedMunch/TimerHelper.cs
+++ src/SevenDigital.FeedMunch/TimerHelper.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
-using System.Threading;
 
 namespace SevenDigital.FeedMunch
 {
@@ -14,28 +12,6 @@ namespace SevenDigital.FeedMunch
 			toTime();
 			stopwatch.Stop();
 			return stopwatch;
-		}
-	}
-
-	public static class ConsoleFilePolling
-	{
-		public static Timer GenerateFileSizePollingTimer(string pathOfFileToPoll, int pollInterval)
-		{
-			return new Timer(x =>
-			{
-				if (!File.Exists(pathOfFileToPoll))
-				{
-					return;
-				}
-
-				var fileInfo = new FileInfo(pathOfFileToPoll);
-				var length = fileInfo.Length;
-				Console.CursorLeft = 0;
-				Console.WriteLine(length);
-				Console.CursorTop --;
-				Console.CursorVisible = false;
-
-			}, null, 0, pollInterval);
 		}
 	}
 }
