2301c6a386e376338b8b7509b61d989fb647ce3c
greg Sochanik <greg@sochanik.com>
2014-02-06 15:44:55 +0000

    Made a start at pulling stream logic out

--- src/FeedMuncher/Program.cs
+++ src/FeedMuncher/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using FeedMuncher.IOC.StructureMap;
-using SevenDigital.Api.FeedReader.Feeds.Schema;
+﻿using FeedMuncher.IOC.StructureMap;
 
 namespace FeedMuncher
 {
@@ -12,22 +10,9 @@ namespace FeedMuncher
 
 			var feedMunchConfig = FeedMunch.Configure.FromConsoleArgs(args);
 			
-			// TODO This would make a good int test
-			//var feedMunchConfig = new FeedMunchConfig
-			//{
-			//	Catalog = FeedCatalogueType.Track,
-			//	Existing = @"H:\New folder\00034_track_full_20140130_licensor.gz",
-			//	Limit = 100,
-			//	Filter = "licensorID=1"
-			//};
-
-			// This is currently hard coded to track, was supposed to be infererred from CatalogType - not sure if this is possible
 			FeedMunch.Download
 				.WithConfig(feedMunchConfig)
 				.Invoke(); // TODO return filepath? 
-#if DEBUG
-			Console.Read();
-#endif
 		}
 	}
 }
