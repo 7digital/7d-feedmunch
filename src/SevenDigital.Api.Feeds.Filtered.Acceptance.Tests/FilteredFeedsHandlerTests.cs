c84f8e675df773e8fc596d9b28a12c70bf44e498
greg Sochanik <greg@sochanik.com>
2014-02-06 21:05:49 +0000

    Added aats for basic feeds filter web service demo

---
+++ src/SevenDigital.Api.Feeds.Filtered.Acceptance.Tests/FilteredFeedsHandlerTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Net;
+using NUnit.Framework;
+using RestSharp;
+
+namespace SevenDigital.Api.Feeds.Filtered.Acceptance.Tests
+{
+	[TestFixture]
+	public class FilteredFeedsHandlerTests
+	{
+		[Test]
+		public void Can_browse_to_artist_full()
+		{
+			var restClient = new RestClient("http://localhost/7d-feeds-filtered/");
+			var restRequest = new RestRequest("artist/full");
+
+			var restResponse = restClient.Get(restRequest);
+
+			Assert.That(restResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			Assert.That(restResponse.Content, Is.EqualTo("I am artist full"));
+		}
+
+		[Test]
+		public void Can_browse_to_track_full()
+		{
+			var restClient = new RestClient("http://localhost/7d-feeds-filtered/");
+			var restRequest = new RestRequest("track/full");
+
+			var restResponse = restClient.Get(restRequest);
+
+			Assert.That(restResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			Assert.That(restResponse.Content, Is.EqualTo("I am track full"));
+		}
+
+		[Test]
+		public void Can_browse_to_release_full()
+		{
+			var restClient = new RestClient("http://localhost/7d-feeds-filtered/");
+			var restRequest = new RestRequest("release/full");
+
+			var restResponse = restClient.Get(restRequest);
+
+			Assert.That(restResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			Assert.That(restResponse.Content, Is.EqualTo("I am release full"));
+		}
+
+		[Test]
+		public void Can_browse_to_artist_update()
+		{
+			var restClient = new RestClient("http://localhost/7d-feeds-filtered/");
+			var restRequest = new RestRequest("artist/updates");
+
+			var restResponse = restClient.Get(restRequest);
+
+			Assert.That(restResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			Assert.That(restResponse.Content, Is.EqualTo("I am artist updates"));
+		}
+
+		[Test]
+		public void Can_browse_to_track_update()
+		{
+			var restClient = new RestClient("http://localhost/7d-feeds-filtered/");
+			var restRequest = new RestRequest("track/updates");
+
+			var restResponse = restClient.Get(restRequest);
+
+			Assert.That(restResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			Assert.That(restResponse.Content, Is.EqualTo("I am track updates"));
+		}
+
+		[Test]
+		public void Can_browse_to_release_update()
+		{
+			var restClient = new RestClient("http://localhost/7d-feeds-filtered/");
+			var restRequest = new RestRequest("release/updates");
+
+			var restResponse = restClient.Get(restRequest);
+
+			Assert.That(restResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			Assert.That(restResponse.Content, Is.EqualTo("I am release updates"));
+		}
+	}
+}
--- src/SevenDigital.Api.Feeds.Filtered/FilteredFeedHandler.cs
+++ src/SevenDigital.Api.Feeds.Filtered/FilteredFeedHandler.cs
@@ -1,15 +1,35 @@
 ﻿using System;
+using System.Collections;
+using System.Net;
 using System.Web;
 
 namespace SevenDigital.Api.Feeds.Filtered
 {
-	public class FilteredFeedHandler : IHttpHandler
+	public class FilteredFeedHandler : HttpHandlerBase, IHttpHandler
+	{
+		public override void ProcessRequest(HttpContextBase context)
+		{
+			var response = context.Response;
+			var segments = new Stack(context.Request.Url.Segments);
+
+			var feedType = ((string)segments.Pop()).TrimEnd('/');
+			var catalogType = ((string)segments.Pop()).TrimEnd('/');
+
+			response.ContentType = "text/plain";
+			response.StatusCode = (int)HttpStatusCode.OK;
+			response.Write(String.Format("I am {0} {1}", catalogType, feedType));
+		}
+	}
+
+	public abstract class HttpHandlerBase
 	{
 		public void ProcessRequest(HttpContext context)
 		{
-			throw new NotImplementedException();
+			ProcessRequest(new HttpContextWrapper(context));
 		}
-		
+
+		public abstract void ProcessRequest(HttpContextBase context);
+
 		public bool IsReusable { get { return true; } }
 	}
 }
