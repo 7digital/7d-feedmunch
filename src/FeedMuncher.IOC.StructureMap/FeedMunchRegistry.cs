551a90fc50aa1b5eb62a0411997a2e510e76e8d6
greg Sochanik <greg@sochanik.com>
2014-02-06 17:51:24 +0000

    Added web service project

---
+++ src/FeedMuncher.IOC.StructureMap/FeedMunchRegistry.cs
@@ -0,0 +1,25 @@
+﻿using SevenDigital.Api.FeedReader;
+using SevenDigital.Api.FeedReader.Configuration;
+using SevenDigital.FeedMunch;
+using StructureMap.Configuration.DSL;
+
+namespace FeedMuncher.IOC.StructureMap
+{
+	public class FeedMunchRegistry : Registry
+	{
+		public FeedMunchRegistry()
+		{
+			Scan(x =>
+			{
+				x.AssemblyContainingType<Feed>();
+				x.AssemblyContainingType<FluentFeedMunch>();
+				x.WithDefaultConventions();
+				x.SingleImplementationsOfInterface();
+			});
+
+			var oAuthConsumerCreds = OAuthConsumerCreds.GenerateFromFile("credentials.txt");
+			For<OAuthConsumerCreds>().Use(oAuthConsumerCreds);
+			For<IFileHelper>().Use<FeedsFileHelper>().Ctor<string>("feedsFolder").Is("feeds").Ctor<string>("outputFolder").Is("output");
+		}
+	}
+}
--- src/SevenDigital.FeedMunch.Integration.Tests.EndToEnd/Full_end_to_end_artist_updates_tests.cs
+++ src/SevenDigital.FeedMunch.Integration.Tests.EndToEnd/Full_end_to_end_artist_updates_tests.cs
@@ -37,4 +37,36 @@ namespace SevenDigital.FeedMunch.Integration.Tests.EndToEnd
 			AssertFiltering.IsAsExpected<ArtistIncremental>(EXPECTED_OUTPUT_FILE, x => x.action == "U");
 		}
 	}
+
+	[TestFixture]
+	[Category("Smoke")]
+	public class Full_end_to_end_artist_tests
+	{
+		private const string OUTPUT_FILE = "artistFullTest";
+		private const string EXPECTED_OUTPUT_FILE = "output/" + OUTPUT_FILE + ".gz";
+
+		[Test]
+		public void Can_filter_action_on_the_fly()
+		{
+			Bootstrap.ConfigureDependencies();
+
+			var feedMunchConfig = new FeedMunchConfig
+			{
+				Catalog = FeedCatalogueType.Artist,
+				Country = "GB",
+				Feed = FeedType.Full,
+				Filter = "name=Interpol,U2",
+				Output = OUTPUT_FILE
+			};
+
+			FeedMuncher.IOC.StructureMap
+					   .FeedMunch.Download
+					   .WithConfig(feedMunchConfig)
+					   .InvokeAndWriteToGzippedFile();
+
+			Assert.That(File.Exists(EXPECTED_OUTPUT_FILE));
+
+			AssertFiltering.IsAsExpected<Artist>(EXPECTED_OUTPUT_FILE, x => x.name == "Interpol" || x.name == "U2");
+		}
+	}
 }
